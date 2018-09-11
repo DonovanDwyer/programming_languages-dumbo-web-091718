5cdf42a65619ce3c57473840fe414c61c7c77a0f
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-11 16:17:52 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,8 @@ def reformat_languages(languages)
       style_arr = []
       type_hash.each do |type_label, type|
         if new_hash.key?(lang)
-          new_hash[lang][:style] = style_arr.push("booty")
+          new_hash[lang][:style] = style_arr.push(style)
+          style_arr << "oi"
         else
           style_arr << style
           new_hash[lang] = {
