c8f6dae1d41959d585e806ff50ecbf725c6a0f94
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-11 15:36:08 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,9 +2,10 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |i, j|
     j.each do |k, l|
-      puts l 
+      new_hash[k] = l
     end
   end
+  puts new_hash
 end
 
 languages = {
